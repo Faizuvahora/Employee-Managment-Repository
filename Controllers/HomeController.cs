68533f17ed1953bdcfa5b82c98fe73b82de0346b
Manthan Lakhlani <lakhlanimanthan55@outlook.com>
2023-01-05 16:59:01 +0530

    Home_Index_Changed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Github.Models;
+﻿using Github.Areas.Identity.Data;
+using Github.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,18 @@ namespace Github.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var result = _userManager.Users.ToList();
+            return View(result);
         }
 
         public IActionResult Privacy()
---
+++ Models/homeIndexModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Github.Models
+{
+    public class homeIndexModel
+    {
+
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Address { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
