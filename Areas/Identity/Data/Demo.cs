12e978679ba5d387a3c1bbd1819ad220b99549ac
Faizan Vahora <faizanvhora03149@outlook.com>
2023-01-06 16:13:32 +0530

    Display User Record To Indext

---
+++ Areas/Identity/Data/Demo.cs
@@ -0,0 +1,10 @@
+﻿namespace Github.Areas.Identity.Data
+{
+    public class Demo
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Address { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
