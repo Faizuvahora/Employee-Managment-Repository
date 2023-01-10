1a8f584b0248bb0445bdff8d28c1116bfc018d61
Manthan Lakhlani <lakhlanimanthan55@outlook.com>
2023-01-10 12:32:29 +0530

    Login_module_changes

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -67,19 +67,19 @@ namespace Github.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [Display(Name = "First Name")]
             public string Fname { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [Display(Name = "Last Name")]
             public string Lname { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [Display(Name = "Address")]
             public string Address { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [Display(Name = "Date Of Birth")]
 
             [DataType(DataType.Date)]
@@ -88,7 +88,7 @@ namespace Github.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -97,7 +97,7 @@ namespace Github.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "This Field is Required")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
